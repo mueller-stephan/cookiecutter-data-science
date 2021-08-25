ffff8615ffefc40136b08c4b6f1506ecf15ca958
An Hoang <anhoang31415@gmail.com>
2021-08-25 11:28:53 +0200

    Update hooks

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,26 @@
 #! /usr/bin/env sh
+
+# Install python version if not exists
+pyenv install -s {{ cookiecutter.python_version }}
+
+# Set local python version
+pyenv local {{ cookiecutter.python_version }}
+export PYENV_VERSION={{ cookiecutter.python_version }}
+eval "$(pyenv init -)"
+
+# Initate Git Repo
 git init
 {% if cookiecutter.git_repo != '' %}
 git remote add origin {{ cookiecutter.git_repo }}
 {% endif %}
 
+# Update Precommit
 pre-commit autoupdate
 make setup
 command -v direnv && direnv allow
+
+# Init Hydra Config
+{% if cookiecutter.use_hydra == 'n' %}
+rm src/cli.py
+rm -r src/configurations
+{% endif %}
